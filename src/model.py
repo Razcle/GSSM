3ac2e425e47a90c64d5d9672bb64ee90cd526481
AVMCMC <annonymous@anon.com>
2019-02-07 21:32:08 +0000

    working kalman filter and smoother

--- src/model.py
+++ src/model.py
@@ -44,8 +44,8 @@ def filter(observations, actions, params):
     :param params: tupple of np arrays
     :return: means and covariances for each time-step
     """
-    N, T, D = observations.shape
-    A, B, C, D, E, d, e = params
+    N, T, dim = observations.shape
+    A, B, C, D, d, E, e = params
 
     def step(f,  F, x, u):
 
@@ -53,25 +53,24 @@ def filter(observations, actions, params):
         mu_h = f @ A + u @ B + d
         mu_x = mu_h @ C + e
         S_hh = A @ F @ A.swapaxes(-2, -1) + D
-        S_vv_inv = np.linalg.inv(B @ S_hh @ B.swapaxes(-2, -1) + E)
-        S_vh = B @ S_hh
+        S_vv_inv = np.linalg.inv(C @ S_hh @ C.swapaxes(-2, -1) + E)
+        S_vh = C @ S_hh
 
         # Use Guassian conditioning to get the filtered posterior on h_t
-        K = S_vh.swapaxes(-2, -1) @ S_vv_inv  # Kalman gain
-        f = mu_h + K @ (x - mu_x)[..., np.newaxis]
-        half = (np.eye(K.shape[0]) - K @ C)
-        F = half @ S_hh @ half.swapaxes(-2, -1) + K @ E @ K.swapaxes(-2, -1)
+        K = S_vh.swapaxes(-2, -1) @ S_vv_inv  # not the Kalman gain
+        f = (mu_h + (K @ (x - mu_x)[:, :, np.newaxis]).swapaxes(-2, -1))[:, 0, :]
+        F = S_hh - S_vh.swapaxes(-2, -1) @ S_vv_inv @ S_vh
 
         return f, F
 
-    means = []
-    covs = []
-    f = np.zeros((N, D))
-    F = np.zeros((N, D, D))
+    means = np.empty((T, N, dim))
+    covs = np.empty((T, N, dim, dim))
+    f = np.ones((N, dim))
+    F = np.ones((N, dim, dim))
     for t in range(T):
         f, F = step(f, F, observations[:, t, :], actions[:, t, :])
-        means.append(f)
-        covs.append(F)
+        means[t] = f
+        covs[t] = F
 
     return means, covs
 
@@ -87,8 +86,8 @@ def smooth(observations, actions, params):
     :return:
     """
 
-    N, T, D = observations.shape
-    A, B, C, D, E, d, e = params
+    N, T, dim = observations.shape
+    A, B, C, D, d, E, e = params
 
     def backward_step(g, G, f, F, u):
 
@@ -101,11 +100,11 @@ def smooth(observations, actions, params):
         Shh_inv = np.linalg.inv(S_hh)
         S_back = F - S_hth.swapaxes(-2, -1) @ Shh_inv @ S_hth
         A_back = S_hth.swapaxes(-2, -1) @ Shh_inv
-        m_back = f - A_back @ mu[..., np.newaxis]
+        m_back = f - (A_back @ mu[..., np.newaxis]).swapaxes(-2, -1)[:,0, :]
 
         # Do the backwards updates
-        crss = A_back @ G + g @ g.swapaxes(-2, 1)  # this cross moment is needed for learning during the M-step of EM
-        g_new = A_back @ g + m_back
+        crss = A_back @ G + g @ g.swapaxes(-2, -1)  # this cross moment is needed for learning during the M-step of EM
+        g_new = (A_back @ g[..., np.newaxis]).swapaxes(-2, -1)[:,0,:] + m_back
         G_new = A_back @ G @ A_back.swapaxes(-2, -1) + S_back
 
         return g_new, G_new, crss
@@ -114,14 +113,14 @@ def smooth(observations, actions, params):
 
     G = covs[-1]
     g = means[-1]
-    new_means = [g]
-    new_covs = [G]
-    crsses = []
+    new_means = np.empty_like(means)
+    new_covs = np.empty_like(covs)
+    crsses = np.empty_like(covs)
     for t in range(T):
-        g, G, cs = backward_step(g, G, means[-1-t], covs[-1-t], actions[:, -1-t,:])
-        new_means.append(g)
-        new_covs.append(G)
-        crsses.append(cs)
+        g, G, cs = backward_step(g, G, means[T-(t+1)], covs[T-(t+1)], actions[:, T-(t+1),:])
+        new_means[T-(t+1)] = g
+        new_covs[T-(t+1)] = G
+        crsses[T-(t+1)] = cs
 
     return new_means, new_covs, crsses
 
--- src/test.py
+++ src/test.py
@@ -1,4 +1,7 @@
 import numpy as np
+from model import filter, smooth
+
+from nose import with_setup
 
 
 def test_vr():
@@ -9,9 +12,79 @@ def test_vr():
     prod = M @ x[..., np.newaxis]
 
     v1 = M[1] @ x[1]
-    print(v1, prod)
-
     np.testing.assert_almost_equal(np.squeeze(prod[1]), v1)
 
 
-test_vr()
+
+def gen_data_for_filtering_and_smoothing():
+    dim = 2
+    T = 500
+    A =  np.array([[np.cos(25), np.sin(25)], [-np.sin(25), np.cos(25)]])
+    B = np.zeros_like(A)
+    C = np.eye(dim)
+    D = 0.1 * np.eye(dim)
+    d = np.zeros(dim)
+    E = 50.0 * np.eye(dim)
+    e = np.zeros(dim)
+
+    params = (A, B, C, D, d, E, e)
+
+    def step(y, u):
+        y = y @ A + u @ B + np.random.multivariate_normal(d, D)
+        return y, y @ C + np.random.multivariate_normal(e, E)
+
+    y = 2 * np.ones((1, dim))
+    u = np.zeros((1, dim))
+    X = np.empty((1, T, dim))
+    Y = np.empty((1, T, dim))
+    for t in range(T):
+        y, x = step(y, u)
+        X[:, t, :] = x
+        Y[:, t, :] = y
+
+    U = np.tile(u, (T, 1))[np.newaxis, ...]
+
+    return X, U, Y, params
+
+X, U, Y_true, params = gen_data_for_filtering_and_smoothing()
+
+
+def test_filter(X, U, params):
+    return filter(X, U, params)
+
+means, covs = test_filter(X, U, params)
+
+import matplotlib.pyplot as plt
+plt.figure()
+plt.plot(X[0, :, 0], X[0, :, 1],'+')
+plt.plot(means[:, 0, 0], means[:, 0, 1], '+r')
+plt.plot(Y_true[0, :, 0], Y_true[0, :, 1], '+g')
+plt.title('filtering')
+plt.show()
+
+plt.figure()
+plt.plot(X[0, :, 0], 'r+')
+plt.plot(Y_true[0, :, 0], 'g+')
+plt.plot(means[:, :, 0], 'r+')
+plt.title('filtering')
+plt.show()
+
+def test_smoother(X, U, params):
+    return smooth(X, U, params)
+
+
+means, covs, crsses = test_smoother(X, U, params)
+
+plt.figure()
+plt.plot(X[0, :, 0], X[0, :, 1],'+')
+plt.plot(means[:, 0, 0], means[:, 0, 1], '+r')
+plt.plot(Y_true[0, :, 0], Y_true[0, :, 1], '+g')
+plt.title('smoothing')
+plt.show()
+
+plt.figure()
+plt.plot(X[0, :, 0], 'r+')
+plt.plot(Y_true[0, :, 0], 'g+')
+plt.plot(means[:, :, 0], 'b+')
+plt.title('smoothing')
+plt.show()
