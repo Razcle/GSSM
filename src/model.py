fixed an off by one error in the smoother and made everyhting more numerically stable

src/model.py
@@ -36,13 +36,16 @@ def M_step(means, covs, crosses, obs, actions, params):
     obs = obs.swapaxes(0, 1)
     A, B, C, D, d, E, e, S_init, mu_init = params
 
+    # Uncentre the covariances
+    covs = covs + means[..., np.newaxis] @ means.reshape(T, N, 1, dim)
+
     # distribution over initial hidden state
     mu_init = np.mean(means[0], axis=0, keepdims=True)
     S_init = covs[0] - mu_init.T @ mu_init
 
     # Transition Matrices
-    A = np.sum(crosses[:-1], axis=0) @ np.linalg.inv(np.sum(covs[:-1], axis=0))
-    C = np.sum(obs[..., np.newaxis] @ means.reshape((T, N, 1, dim)), axis=0)
+    A = np.sum(crosses[1:] @ np.linalg.inv(np.sum(covs[:-1], axis=0)), axis=0)
+    C = obs[..., np.newaxis] @ means.reshape((T, N, 1, dim))
     C = np.sum(C @  np.linalg.inv(np.sum(covs, axis=0)), axis=0)
 
     # Noise Matrices
@@ -58,11 +61,11 @@ def M_step(means, covs, crosses, obs, actions, params):
     return params
 
 
-def log_liklihood(mean, prec, obs):
-    s, absZ = np.linalg.slogdet(prec)
-    Z = 0.5 * s * absZ
+def log_liklihood(mean, cov, obs):
+    s, absZ = np.linalg.slogdet(cov)
+    Z = - 0.5 * s * absZ
     delta_x = obs - mean
-    half = prec @ delta_x[:, :, np.newaxis]
+    half = np.linalg.solve(cov, delta_x[:, :, np.newaxis])
     mahalob = - 0.5 * np.expand_dims(delta_x, 1) @ half
     return np.squeeze(Z + mahalob)
 
@@ -81,23 +84,21 @@ def filter(observations, actions, params):
 
     # checkshapes(params, N, T , dim)
 
-    def step(f,  F, x, u, ll):
+    def step(f,  F, x, u):
 
         # find the means and covariances of the joint p(v_t, h_t|x_:t)
         mu_h = f @ A + u @ B + d
         mu_x = mu_h @ C + e
         S_hh = A @ F @ A.swapaxes(-2, -1) + D
         S_vv = C @ S_hh @ C.swapaxes(-2, -1) + E
-        S_vv_inv = np.linalg.inv(S_vv)
         S_vh = C @ S_hh
 
         # Use Guassian conditioning to get the filtered posterior on h_t
-        K = S_vh.swapaxes(-2, -1) @ S_vv_inv  # not the Kalman gain
-        f = (mu_h + (K @ (x - mu_x)[:, :, np.newaxis])[:, :, 0])
-        F = S_hh - S_vh.swapaxes(-2, -1) @ S_vv_inv @ S_vh
+        f = mu_h + (S_vh.swapaxes(-2, -1) @ np.linalg.solve(S_vv, (x - mu_x)[:, :, np.newaxis]))[:, :, 0]
+        F = S_hh - S_vh.swapaxes(-2, -1) @ np.linalg.solve(S_vv, S_vh)
 
         # Calculate the probability of this observation
-        ll = log_liklihood(mu_x, S_vv_inv, x)
+        ll = log_liklihood(mu_x, S_vv, x)
 
         return f, F, ll
 
@@ -107,7 +108,7 @@ def filter(observations, actions, params):
     F = S_init
     loglik = 0.0
     for t in range(T):
-        f, F , ll = step(f, F, observations[:, t, :], actions[:, t, :], loglik)
+        f, F, ll = step(f, F, observations[:, t, :], actions[:, t, :])
         means[t] = f
         covs[t] = F
         loglik += ll
@@ -129,23 +130,22 @@ def smooth(observations, actions, params):
     N, T, dim = observations.shape
     A, B, C, D, d, E, e, _, _ = params
 
-    def backward_step(g, G, f, F, u):
+    def backward_step(g, G, f, F):
 
         # Get moments of join to then condition
-        mu = f @ A + u @ B + d
+        mu = f @ A + d
         S_hth = A @ F
         S_hh = S_hth @ A.swapaxes(-2, -1) + D
 
         # Set up backwards dynamics
-        Shh_inv = np.linalg.inv(S_hh)
-        S_back = F - S_hth.swapaxes(-2, -1) @ Shh_inv @ S_hth
-        A_back = S_hth.swapaxes(-2, -1) @ Shh_inv
+        S_back = F - S_hth.swapaxes(-2, -1) @ np.linalg.solve(S_hh, S_hth)
+        A_back = np.linalg.solve(S_hh, S_hth).swapaxes(-2, -1)
         m_back = f - (A_back @ mu[..., np.newaxis])[:,:,0]
 
         # Do the backwards updates
         g_new = (A_back @ g[..., np.newaxis])[:,:,0] + m_back
         G_new = A_back @ G @ A_back.swapaxes(-2, -1) + S_back
-        crss = A_back @ G_new + g @ g_new.swapaxes(-2, -1)  # this cross moment is needed for learning during the M-step of EM
+        crss = A_back @ G + g_new @ g.swapaxes(-2, -1)  # this cross moment is needed for learning during the M-step of EM
 
         return g_new, G_new, crss
 
@@ -153,16 +153,21 @@ def smooth(observations, actions, params):
 
     G = covs[-1]
     g = means[-1]
+    cs = covs[-1]
     new_means = np.empty_like(means)
     new_covs = np.empty_like(covs)
-    crsses = np.empty_like(covs)
-    for t in range(T):
-        g, G, cs = backward_step(g, G, means[T-(t+1)], covs[T-(t+1)], actions[:, T-(t+1),:])
-        new_means[T-(t+1)] = g
-        new_covs[T-(t+1)] = G
-        crsses[T-(t+1)] = cs
-
+    new_means[-1] = g
+    new_covs[-1] = G
+    crsses = np.empty((T - 1, N, dim, dim))
+    print(new_means[0])
+    for t in range(T-2, -1, -1):
+        g, G, cs = backward_step(g, G, means[t], covs[t])
+        new_means[t] = g
+        new_covs[t] = G
+        crsses[t] = cs
+    print(new_means[0])
     return new_means, new_covs, crsses, loglik
+
 
 def checkshapes(params, N, T, dim):
     A, B, C, D, d, E, e, S_init, mu_init = params

src/test.py
@@ -1,7 +1,6 @@
 import numpy as np
 from model import filter, smooth, EM
 
-from nose import with_setup
 
 
 def test_vr():
@@ -18,16 +17,16 @@ def test_vr():
 
 def gen_data_for_filtering_and_smoothing():
     dim = 2
+    theta = 40.0 * 180/np.pi
     T = 500
-    mu_init =  np.ones((1,dim))
+    mu_init = np.zeros((1,dim))
     S_init = np.eye(dim)
-    A = np.eye(dim)
-        # np.array([[np.cos(25), np.sin(25)], [-np.sin(25), np.cos(25)]])
+    A = 1.000001 * np.eye(dim)
     B = np.zeros_like(A)
     C = np.eye(dim)
-    D = np.eye(dim)
+    D = 0.5 * np.eye(dim)
     d = np.zeros(dim)
-    E =  np.eye(dim)
+    E = 1.5 * np.eye(dim)
     e = np.zeros(dim)
 
     params = (A, B, C, D, d, E, e, S_init, mu_init)
@@ -52,26 +51,34 @@ def gen_data_for_filtering_and_smoothing():
 X, U, Y_true, params = gen_data_for_filtering_and_smoothing()
 
 
-# def test_filter(X, U, params):
-#     return filter(X, U, params)
-#
-# means, covs, loglik = test_filter(X, U, params)
-# print(loglik)
-#
+def test_filter(X, U, params):
+    return filter(X, U, params)
+
+means, covs, loglik = test_filter(X, U, params)
+print(loglik)
+
 import matplotlib.pyplot as plt
-# plt.figure()
-# plt.plot(X[0, :, 0], X[0, :, 1],'+k')
-# plt.plot(means[:, 0, 0], means[:, 0, 1], '+r')
-# plt.plot(Y_true[0, :, 0], Y_true[0, :, 1], '+g')
-# plt.title('filtering')
-# plt.show()
-#
-# plt.figure()
-# plt.plot(X[0, :, 0], 'k+')
-# plt.plot(Y_true[0, :, 0], 'g+')
-# plt.plot(means[:, :, 0], 'r+')
-# plt.title('filtering')
-# plt.show()
+plt.figure()
+plt.plot(X[0, :, 0], X[0, :, 1],'+k')
+plt.plot(means[:, 0, 0], means[:, 0, 1], '+r')
+plt.plot(Y_true[0, :, 0], Y_true[0, :, 1], '+g')
+plt.title('filtering')
+plt.show()
+
+plt.figure()
+plt.plot(X[0, :, 0], 'k+')
+plt.plot(Y_true[0, :, 0], 'g+')
+plt.plot(means[:, :, 0], 'r+')
+plt.title('filtering')
+plt.show()
+
+plt.figure()
+plt.plot(X[0, :, 1], 'k+')
+plt.plot(Y_true[0, :, 1], 'g+')
+plt.plot(means[:, :, 1], 'b+')
+plt.title('filtering')
+plt.show()
+
 
 def test_smoother(X, U, params):
     return smooth(X, U, params)
@@ -94,9 +101,17 @@ plt.plot(means[:, :, 0], 'b+')
 plt.title('smoothing')
 plt.show()
 
+plt.figure()
+plt.plot(X[0, :, 1], 'k+')
+plt.plot(Y_true[0, :, 1], 'g+')
+plt.plot(means[:, :, 1], 'b+')
+plt.title('smoothing')
+plt.show()
+
 #
+# #
 # def callback(ll, params):
-#     print(ll)
+#     print(ll, params)
 #
 #
 # learned_params = EM(params, X, U, callback=callback, tol=1e-1)